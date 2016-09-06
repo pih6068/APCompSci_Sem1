fbb10e7ef5a0c3d828f87c73084a93225b33bef9
pih6068 <tiantian.pi@gmail.com>
2016-09-06 08:07:12 -0700

    Added Files

---
+++ APLesson_01/asciiArt1.java
@@ -0,0 +1,9 @@
+public class asciiArt1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  /\\_/\\ ");
+		System.out.println(" ( o.o ) ");
+		System.out.println("  > ^ < ");
+	}
+}                      
