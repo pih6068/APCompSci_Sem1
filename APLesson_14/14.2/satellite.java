dc752e90b515112ff1ac4a2863380795cd1e4e3a
pih6068 <tiantian.pi@gmail.com>
2017-05-24 13:18:28 -0700

    updated 14.2, added picture

--- APLesson_14/14.2/satellite.java
+++ APLesson_14/14.2/satellite.java
@@ -3,42 +3,48 @@ public class satellite
 {
    public static void main(String[]args)
    {
-       ArrayList<location> locate = new ArrayList<>();
-       double[] honLoc = {5, 6};
-       locate.add(new honda(honLoc));
-       locate.add(new toyota("8, 9"));
-       locate.add(new gmc(3, 8));
+      ArrayList<location> locate = new ArrayList<>();
+      double[] honLoc = {5, 6};
+      locate.add(new honda(honLoc));
+      locate.add(new toyota("8, 9"));
+      locate.add(new gmc(3, 8));
 
-        double[] home = {0, 0};
+      double[] home = {0, 0};
 
-        String printout = "\n\n" +
-               "==========================" + "\nStarting locations...";
+      String printout = "\n\n" +
+            "==========================" + "\nStarting locations...";
 
-        for (location location : locate)
-       {
-           printout += "\nLocation for " + location.getID() + ": (" + getLocation(location.getLoc()) + ")";
-       }
+      for (location location : locate)
+      {
+          printout += "\nLocation for " + location.getID() + ": (" + getLocation(location.getLoc()) + ")";
+      }
 
-		
-		for(location location: locate)
-		{
-		double x,y;
-		x = (Math.random() * 100 + 1);
-		y = (Math.random() * 100 + 1);
-		System.out.println("After " + location.getID() + " moved ("+x+","+y+")");
-		location.move(x,y);
-		System.out.println("new location ("+location.getLoc()[0]+","+location.getLoc()[1]+")");
-		}
-		
-		printout += "\n\n" + "==========================" +
+       printout += "\n\n" + "==========================" +
                    "\nDistance from home...";
 
-        for (location location : locate)
-       {
-           printout += "\nDistance for " + location.getID() + ": (" + getDistance(location.getLoc(), home)+ ")";
-       }
 
-        System.out.println(printout);
+      for (location location : locate)
+      {
+          printout += "\nDistance for " + location.getID() + ": (" + getDistance(location.getLoc(), home)+ ")";
+      }
+  
+      printout += "\n\n" + "==========================";
+   
+      System.out.println(printout);
+		  for(location location: locate)
+		  {
+		      double x,y;
+		      x = (Math.random() * 100 + 1);
+		      y = (Math.random() * 100 + 1);
+		      System.out.println("After " + location.getID() + " moved ("+x+","+y+")");
+		      location.move(x,y);
+		      System.out.println("new location ("+location.getLoc()[0]+","+location.getLoc()[1]+")");
+		  }
+	
+
+      
+
+
    }
 
     public static double getDistance(double[] car, double[] home)
